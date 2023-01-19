74722c284e34580b74cab18af9b3063fac655efa
iv-kovatchev <iv.kovatchev@gmail.com>
2023-01-20 00:51:04 +0200

    add blog project again

---
+++ blog/src/actions/index.js
@@ -0,0 +1,23 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from 'lodash';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    console.log(getState());
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+    console.log(getState());
+};
+
+export const fetchPosts = () =>
+    async (dispatch) => {
+        const response = await jsonPlaceholder.get('/posts');
+
+        dispatch({type: 'FETCH_POSTS', payload: response.data})
+    };
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({type: 'FETCH_USER', payload: response.data});
+}
---
+++ blog/src/components/App.js
@@ -0,0 +1,11 @@
+import PostsList from "./PostsList";
+
+const App = () => {
+    return (
+        <div className="ui container">
+            <PostsList />
+        </div>
+    )
+}
+
+export default App;
